b276df27ea51ca68c066e29b0757f2e45cd86840
unknown <heyreza8@gmail.com>
2025-02-19 23:18:05 +0330

    Feat Post API Service

---
+++ services/PostService.js
@@ -0,0 +1,21 @@
+import Http from "@/http/http";
+
+export const GetAllPosts = () => {
+  return Http.get(`/post`);
+};
+
+export const GetPost = (ID) => {
+  return Http.get(`/post/${ID}`);
+};
+
+export const CreatePost = (postData) => {
+  return Http.post(`/post/`, postData);
+};
+
+export const UpdatePost = (ID, postData) => {
+  return Http.post(`/post/update/${ID}`, postData);
+};
+
+export const DeletePost = (ID) => {
+  return Http.post(`/post/delete/${ID}`);
+};
